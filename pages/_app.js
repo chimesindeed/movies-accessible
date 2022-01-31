7455cd86dff894e903a28d7435ec21e5f9341305
Dimitri Gedda <gratitudeadore@fastmail.com>
2022-01-31 03:38:49 -0500

    Added navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import NavBar from '../components/NavBar'
 
 function MyApp({ Component, pageProps }) {
 
@@ -11,9 +12,9 @@ function MyApp({ Component, pageProps }) {
         <link rel="stylesheet" 
               href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
               integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-              crossorigin="anonymous" />
+              crossOrigin="anonymous" />
       </Head>
-      
+      <NavBar />
       <Component {...pageProps} />
     </>
   )
