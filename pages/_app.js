dafd03ff2a314aa930f075be583281f5edf773e3
Dimitri Gedda <gratitudeadore@fastmail.com>
2022-01-31 02:53:02 -0500

    Added bootstrap cdn to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="stylesheet" 
+              href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+              integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+              crossorigin="anonymous" />
+      </Head>
+      
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
