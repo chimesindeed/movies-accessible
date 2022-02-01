f308f019a366ede77fad722525c232b99fa71b7c
Dimitri Gedda <gratitudeadore@fastmail.com>
2022-02-01 16:44:30 -0500

    Added styling to '/movies' page

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -1,29 +1,47 @@
-import { useContext } from 'react'
+import styles from './movies.module.css'
+import {useRouter} from 'next/router'
 import ApiData from '../../context'
 
 function Movies({movies}){
+    const router = useRouter();
+    
+    const list = movies.d
+    return (
 
-    const [apiData, setApiData] = useContext(ApiData)
-    return (<h1>Movies</h1>)
+        <div id="movies" className={styles.movies}>
+            {
+                list.map(item => {
+                    const name = item.l;
+                    const poster = item.i.imageUrl;
+                    return (
+                        <li key={item.id}>
+                            <img src={poster}/>
+                            <h2>{name}</h2>
+                        </li>
+                    )
+                })
+            }
+        </div>
+        )
 }
 export default Movies
 
-
 export async function getServerSideProps(context){
-    console.log(context.query.query)
-    console.log(process.env.API_HOST)
-    const response = await fetch("https://imdb8.p.rapidapi.com/auto-complete?q=" + context.query.query, {
-        "method": "GET",
-        "headers": {
+    if (context.query.query){ 
+        const response = await fetch("https://imdb8.p.rapidapi.com/auto-complete?q=" + context.query.query, {
+            "method": "GET",
+            "headers": {
                 "x-rapidapi-host": process.env.API_HOST,
                 "x-rapidapi-key": process.env.API_KEY
-        }
-    })
-    const data = await response.json()
+            }
+        })
     
-    return {
-        props: {
-            movies: data
+        const data = await response.json()
+        context.query.query = null
+        return {
+            props: {
+                movies: data
+            }
         }
     }
 }
